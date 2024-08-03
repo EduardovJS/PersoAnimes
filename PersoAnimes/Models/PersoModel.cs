61756084f339e1e7de615b58fad9ae502514972b
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-08-03 20:02:00 -0300

    Tela de listagem de personagens

--- PersoAnimes/Models/PersoModel.cs
+++ PersoAnimes/Models/PersoModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string NomeAnime { get; set; } = string.Empty;
-        public string Nome { get; set; } = string.Empty;
+        public string Capa { get; set; } = string.Empty;
         public string Autor { get; set; } = string.Empty;
         public string Descricao { get; set; } = string.Empty;
 
